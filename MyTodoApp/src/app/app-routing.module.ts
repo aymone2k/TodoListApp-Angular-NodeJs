2198efae558738363d83964abf7d20d04852ce99
2kalm <aymone.koissy@gmail.com>
2021-08-19 09:32:44 +0200

    initialisation angular + routage

--- MyTodoApp/src/app/app-routing.module.ts
+++ MyTodoApp/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { EditCategoryComponent } from './components/categories/edit-category/edit-category.component';
+import { AddTodoComponent } from './components/todolist/add-todo/add-todo.component';
+import { DetailTodoComponent } from './components/todolist/detail-todo/detail-todo.component';
+import { EditTodoComponent } from './components/todolist/edit-todo/edit-todo.component';
+import { TodolistComponent } from './components/todolist/todolist.component';
+import { TodosComponent } from './components/todolist/todos/todos.component';
+import { EditProfilComponent } from './components/users/edit-profil/edit-profil.component';
+import { ResetPasswordComponent } from './components/users/reset-password/reset-password.component';
+import { SignInComponent } from './components/users/sign-in/sign-in.component';
+import { SignUpComponent } from './components/users/sign-up/sign-up.component';
+import { HomeComponent } from './view/home/home.component';
+import { NotFoundComponent } from './view/not-found/not-found.component';
+import { TodoModalComponent } from './view/todo-modal/todo-modal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'signin', component: SignInComponent},
+  {path: 'signup', component: SignUpComponent},
+  {path: 'edit-profil/:id', component: EditProfilComponent},
+  {path: 'reset-password', component: ResetPasswordComponent},
+  {path: 'todolist', component: TodolistComponent},
+  {path: 'todo-modal/:id', component: TodoModalComponent},
+  {path: 'edit-todo/:id', component: EditTodoComponent},
+  {path: 'detail-todo/:id', component: DetailTodoComponent},
+  {path: 'categories', component: CategoriesComponent},
+  {path:'edit-category/:id', component: EditCategoryComponent},
+  {path: 'not-found', component:NotFoundComponent },
+  {path: '**', pathMatch:'full', redirectTo:'not-found'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MyTodoApp/src/app/app.module.ts
+++ MyTodoApp/src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './view/header/header.component';
+import { FooterComponent } from './view/footer/footer.component';
+import { HomeComponent } from './view/home/home.component';
+import { TodoModalComponent } from './view/todo-modal/todo-modal.component';
+import { NotFoundComponent } from './view/not-found/not-found.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    TodoModalComponent,
+    NotFoundComponent,
+
   ],
   imports: [
     BrowserModule,
